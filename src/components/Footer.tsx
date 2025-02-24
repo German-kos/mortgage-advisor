ad0063e2a5b5a759c732c84bc93def0ae76349b5
Tal <talmeiso@gmail.com>
2025-02-24 18:07:53 +0200

    big dingus

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,47 +1,38 @@
 import {
-  footerContainer,
-  footerLogoContainer,
-  footerLogo,
-  footerCredits,
-  socialLinksContainer,
-  socialIcon,
-  footerWrapper,
-} from "../styles/footerStyles";
-import {
+  FaFacebook,
   FaInstagram,
   FaLinkedin,
-  FaWhatsapp,
-  FaFacebook,
   FaTwitter,
+  FaWhatsapp,
 } from "react-icons/fa";
 
 type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <footer className={footerWrapper}>
-      <div className={footerContainer}>
+    <footer className="w-full py-6 bg-orange-100">
+      <div className="flex flex-col items-center justify-between max-w-6xl px-6 mx-auto md:flex-row sm:px-12 md:px-20">
         {/* Logo */}
-        <div className={footerLogoContainer}>
-          <div className={footerLogo}>הלוגו של אלירן</div>
-          <p className={footerCredits}>נבנה ע"י גרמן קוסטיאקוב</p>
+        <div className="flex flex-col items-center sm:items-start">
+          <div className="flex items-center justify-center mb-2 text-black">הלוגו של אלירן</div>
+          <p className="text-sm text-stone-500">נבנה ע"י גרמן קוסטיאקוב</p>
         </div>
 
         {/* Socials */}
-        <div className={socialLinksContainer}>
-          <a href="#" className={socialIcon}>
+        <div className="flex gap-4 mt-4 sm:mt-0">
+          <a href="#" className="w-8 h-8 transition text-emerald-800 hover:text-emerald-600">
             <FaInstagram size={32} />
           </a>
-          <a href="#" className={socialIcon}>
+          <a href="#" className="w-8 h-8 transition text-emerald-800 hover:text-emerald-600">
             <FaLinkedin size={32} />
           </a>
-          <a href="#" className={socialIcon}>
+          <a href="#" className="w-8 h-8 transition text-emerald-800 hover:text-emerald-600">
             <FaWhatsapp size={32} />
           </a>
-          <a href="#" className={socialIcon}>
+          <a href="#" className="w-8 h-8 transition text-emerald-800 hover:text-emerald-600">
             <FaFacebook size={32} />
           </a>
-          <a href="#" className={socialIcon}>
+          <a href="#" className="w-8 h-8 transition text-emerald-800 hover:text-emerald-600">
             <FaTwitter size={32} />
           </a>
         </div>
