0f7987a62b395561a89bebcd856a753d87d4a738
German Kostiakov <KostiakovG@gmail.com>
2025-02-25 20:40:35 +0200

    persistent whatsapp icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,25 @@
 import { FaWhatsapp } from "react-icons/fa";
 
-export const Navbar = () => {
+export const Navbar: React.FC = () => {
   return (
-    <div className="w-full bg-orange-100">
-      {/* Wrapper to match Hero section width and padding */}
-      <div className="w-full h-16 px-6 sm:px-12 md:px-20">
-        {" "}
-        {/* Example height: 64px (16 Tailwind units) */}
-        <nav className="flex items-center justify-between w-full py-4 mx-auto max-w-7xl">
+    <div className="w-full px-6 bg-orange-100 sm:px-12 md:px-20">
+      <div className="relative w-full mx-auto max-w-7xl">
+        <nav className="flex items-center justify-between py-4">
           {/* Logo */}
           <div className="text-2xl font-bold text-black">הלוגו של אלירן</div>
-          {/* WhatsApp Icon */}
-          <div className="flex items-center gap-6">
-            <a
-              className="text-green-500 transition hover:text-green-600"
-              href="https://wa.me/972547963334"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <FaWhatsapp size={48} />
-            </a>
-          </div>
         </nav>
+      </div>
+
+      {/* WhatsApp Icon */}
+      <div className="fixed z-50 top-4 left-6 sm:left-12 md:left-20">
+        <a
+          className="text-green-500 transition hover:text-green-600"
+          href="https://wa.me/972547963334"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaWhatsapp size={44} strokeWidth={5} />
+        </a>
       </div>
     </div>
   );
