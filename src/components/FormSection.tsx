40dcd58f24ca04b0e3e893b167bee05c7e8f2de5
German Kostiakov <KostiakovG@gmail.com>
2025-03-08 00:49:34 +0200

    padding changes

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -5,7 +5,7 @@ import { Form } from "./Form";
 
 export const FormSection = () => {
   return (
-    <CustomBox className="py-8 sm:py-16 lg:py-24">
+    <CustomBox className="py-8 sm:py-16">
       <CustomContainer>
         <Grid className="sm:justify-center min-[400px]:gap-16 sm:grid grid-cols-1 lg:grid-cols-2 grid-rows-2 lg:grid-rows-1 text-center lg:text-right">
           <div className="flex justify-center sm:items-center sm:h-auto sm:max-h-none">
