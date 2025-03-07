36e31a4ad6e23d4c01cd56d163565681ef771fb0
German Kostiakov <KostiakovG@gmail.com>
2025-03-08 01:57:48 +0200

    button style changes

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -2,7 +2,7 @@ import { Button } from "@radix-ui/themes/src/index.js";
 
 export const PrimaryButton: React.FC<{ text: string }> = ({ text }) => {
   return (
-    <Button className="bg-[#F39C12] px-5 py-[9px] border rounded-[8px] min-[500px]:max-w-32 font-semibold text-[16px] text-white">
+    <Button className="bg-[#F39C12] hover:bg-[#f36c12] px-5 py-[9px] border rounded-[8px] min-[500px]:max-w-32 font-semibold text-[16px] text-white transition">
       {text}
     </Button>
   );
