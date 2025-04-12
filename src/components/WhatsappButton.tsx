7a505e48f219047c91f5016458a91ea03f190010
German <kostiakovg@gmail.com>
2025-04-12 11:05:33 +0300

    whatsapp message update

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -9,14 +9,14 @@ export const WhatsappButton = () => {
 
       <Link
         className="relative rounded-full focus:outline-none transition-transform"
-        href="https://wa.me/972547963334"
+        href="https://wa.me/972547963334?text=היי,%20אני%20אשמח%20שנדבר%20על%20המשכנתא%20:)"
         target="_blank"
         rel="noreferrer"
       >
         <img
           src="/images/Whatsapp.png"
           alt="whatsapp"
-          className="whatsapp-beat w-16 md:w-20 lg:w-24 h-16 md:h-20 lg:h-24"
+          className="w-16 md:w-20 lg:w-24 h-16 md:h-20 lg:h-24 whatsapp-beat"
         />
       </Link>
     </div>
