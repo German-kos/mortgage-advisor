4e1015ec1b89b13bac3a5fc2b671a5bf94171c1c
German Kostiakov <KostiakovG@gmail.com>
2025-04-18 10:08:33 +0300

    comment removed

--- send-email.js
+++ send-email.js
@@ -3,8 +3,8 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
-  to: "kostiakovg@gmail.com", // Change this to the client's actual email
-  from: "kostiakovg@gmail.com", // Must be verified in SendGrid
+  to: "kostiakovg@gmail.com",
+  from: "kostiakovg@gmail.com",
   subject: "New Form Submission",
   text: "Someone submitted the contact form!",
   html: "<strong>You just got a new lead 🎉</strong>",
