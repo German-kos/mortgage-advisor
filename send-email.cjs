38438b69d3cabe1fd1c1640fd57fc34fee7e193f
German Kostiakov <KostiakovG@gmail.com>
2025-04-18 10:36:07 +0300

    action test

--- send-email.cjs
+++ send-email.cjs
@@ -3,7 +3,7 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
-  to: "germaskk@gmail.com",
+  to: "kostiakovg@gmail.com",
   from: "kostiakovg@gmail.com",
   subject: "New Form Submission",
   text: "Someone submitted the contact form!",
