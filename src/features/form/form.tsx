55e7f3d530b1b32b976a811a39025b5c4c04d010
German Kostiakov <KostiakovG@gmail.com>
2025-04-19 15:40:38 +0300

    better animations

--- src/features/form/form.tsx
+++ src/features/form/form.tsx
@@ -9,7 +9,7 @@ import { fullNameFields } from "./form-config";
 import { sendToGitHub } from "./utils/send-to-github";
 import { useFormStatus } from "@/features/form/context/form-status-context";
 import { FormFeedback } from "./components/form-feedback";
-
+import { AnimatePresence, motion } from "framer-motion";
 type FormProps = {
   showSubtext?: boolean;
 };
@@ -57,6 +57,7 @@ export const Form = ({ showSubtext = true }: FormProps) => {
 
     try {
       await sendToGitHub({ name, email, phone, time });
+      await new Promise((res) => setTimeout(res, 500));
       setStatus("success");
       form.reset();
     } catch (err) {
@@ -65,95 +66,122 @@ export const Form = ({ showSubtext = true }: FormProps) => {
     }
   };
 
-  if (status !== "idle") return <FormFeedback />;
+  // if (status !== "idle") return <FormFeedback />;
 
   return (
     <Box className="flex flex-col lg:justify-between md:gap-4 xl:gap-5 max-w-[480px] h-full">
-      <div className="gap-2 md:gap-4 xl:gap-5 grid max-w-md">
-        <h2 className="font-semibold text-[#101828] text-3xl xl:text-4xl">
-          קבל ייעוץ חינם
-        </h2>
-        {showSubtext && (
-          <p className="font-normal text-[#475467] text-lg">
-            הצוות שלנו ישמח לשוחח איתך ולעזור לך למצוא את הפתרון הטוב ביותר.
-          </p>
-        )}
-      </div>
-
-      <RadixForm.Root className="pb-6 md:pb-0 w-full" onSubmit={handleSubmit}>
-        <Grid className="gap-4 md:gap-6 grid">
-          {/* Name Fields */}
-          <div className="gap-8 grid grid-cols-2 grid-rows-1">
-            {fullNameFields.map((field, index) => (
-              <CustomRadixField
-                key={index}
-                name={field.name}
-                text={field.label}
-                type={field.type}
-                placeholder={`${field.label}...`}
-              />
-            ))}
-          </div>
-
-          {/* Phone */}
-          <CustomRadixField
-            key="phone"
-            name="phone"
-            text="טלפון"
-            type="tel"
-            placeholder="0501234567"
-          />
-
-          {/* Email */}
-          <CustomRadixField
-            key="email"
-            name="email"
-            text="אימייל"
-            type="email"
-            placeholder="example@mail.com"
-            mismatch={true}
-            mismatchText="כתובת אימייל לא תקינה *"
-            badEmail={showEmailError}
-            setBadEmail={setShowEmailError}
-          />
-
-          {/* Terms & Conditions */}
-          <Grid className="flex flex-col items-start gap-2">
-            <div className="flex items-center gap-3">
-              <input
-                type="checkbox"
-                id="c1"
-                checked={isChecked}
-                onChange={(e) => {
-                  setIsChecked(e.target.checked);
-                  setShowError(false); // Hide error if checked
-                }}
-                className="border border-[#D0D5DD] rounded w-5 h-5 cursor-pointer"
-              />
-              <label
-                className="font-normal text-base text-right leading-none"
-                htmlFor="c1"
-              >
-                אתה מסכים למדיניות הפרטיות הידידותית שלנו.
-              </label>
+      <AnimatePresence mode="wait" initial={false}>
+        {status === "idle" && (
+          <motion.div
+            key="form"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            <div className="gap-2 md:gap-4 xl:gap-5 grid max-w-md">
+              <h2 className="font-semibold text-[#101828] text-3xl xl:text-4xl">
+                קבל ייעוץ חינם
+              </h2>
+              {showSubtext && (
+                <p className="font-normal text-[#475467] text-lg">
+                  הצוות שלנו ישמח לשוחח איתך ולעזור לך למצוא את הפתרון הטוב
+                  ביותר.
+                </p>
+              )}
             </div>
+            <RadixForm.Root
+              className="pb-6 md:pb-0 w-full"
+              onSubmit={handleSubmit}
+            >
+              <Grid className="gap-4 md:gap-6 grid">
+                {/* Name Fields */}
+                <div className="gap-8 grid grid-cols-2 grid-rows-1">
+                  {fullNameFields.map((field, index) => (
+                    <CustomRadixField
+                      key={index}
+                      name={field.name}
+                      text={field.label}
+                      type={field.type}
+                      placeholder={`${field.label}...`}
+                    />
+                  ))}
+                </div>
 
-            {/* Error Message */}
-            {showError && (
-              <p className="mt-1 text-red-500 text-sm">
-                יש לאשר את התנאים לפני שליחת הטופס.
-              </p>
-            )}
-          </Grid>
+                {/* Phone */}
+                <CustomRadixField
+                  key="phone"
+                  name="phone"
+                  text="טלפון"
+                  type="tel"
+                  placeholder="0501234567"
+                />
 
-          {/* Submit Button */}
-          <RadixForm.Submit asChild>
-            <Button className="bg-[#F39C12] hover:bg-[#f36c12] mx-auto rounded-lg w-full h-[45px] font-semibold text-white text-base transition">
-              בקשת שיחה חינם
-            </Button>
-          </RadixForm.Submit>
-        </Grid>
-      </RadixForm.Root>
+                {/* Email */}
+                <CustomRadixField
+                  key="email"
+                  name="email"
+                  text="אימייל"
+                  type="email"
+                  placeholder="example@mail.com"
+                  mismatch={true}
+                  mismatchText="כתובת אימייל לא תקינה *"
+                  badEmail={showEmailError}
+                  setBadEmail={setShowEmailError}
+                />
+
+                {/* Terms & Conditions */}
+                <Grid className="flex flex-col items-start gap-2">
+                  <div className="flex items-center gap-3">
+                    <input
+                      type="checkbox"
+                      id="c1"
+                      checked={isChecked}
+                      onChange={(e) => {
+                        setIsChecked(e.target.checked);
+                        setShowError(false); // Hide error if checked
+                      }}
+                      className="border border-[#D0D5DD] rounded w-5 h-5 cursor-pointer"
+                    />
+                    <label
+                      className="font-normal text-base text-right leading-none"
+                      htmlFor="c1"
+                    >
+                      אתה מסכים למדיניות הפרטיות הידידותית שלנו.
+                    </label>
+                  </div>
+
+                  {/* Error Message */}
+                  {showError && (
+                    <p className="mt-1 text-red-500 text-sm">
+                      יש לאשר את התנאים לפני שליחת הטופס.
+                    </p>
+                  )}
+                </Grid>
+
+                {/* Submit Button */}
+                <RadixForm.Submit asChild>
+                  <Button className="bg-[#F39C12] hover:bg-[#f36c12] mx-auto rounded-lg w-full h-[45px] font-semibold text-white text-base transition">
+                    בקשת שיחה חינם
+                  </Button>
+                </RadixForm.Submit>
+              </Grid>
+            </RadixForm.Root>
+          </motion.div>
+        )}
+        {status !== "idle" && (
+          <motion.div
+            className="flex justify-center items-center w-full h-full"
+            key="feedback"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            <FormFeedback />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </Box>
   );
 };
