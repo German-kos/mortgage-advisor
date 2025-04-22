8dda1e8eae1615fd226d078ccaf4ff9917ba3dab
German <kostiakovg@gmail.com>
2025-04-22 19:26:26 +0300

    test fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
-export { default as Logo } from "./logo";
 export { default as CustomBox } from "./custom-box";
 export { default as CustomContainer } from "./custom-container";
 export { default as PrimaryButton } from "./primary-button";
 export { Modal } from "./modal";
+export { default as Logo } from "./logo";
